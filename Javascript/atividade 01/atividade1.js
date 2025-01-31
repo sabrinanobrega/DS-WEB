e0a221de83cf57d631acb3b7b77a36b4a18ac5a8
sabrinanobrega <sabrinanobrega90@gmail.com>
2025-01-31 15:17:59 -0300

    Atividade 01

---
+++ Javascript/atividade 01/atividade1.js
@@ -0,0 +1,43 @@
+//Escreva em JavaScript um programa que mostre na tela a mensagem “Bom dia,Mundo!!!”()
+window.alert("olá mundo")
+
+//Escreve em JavaScript um programa que receba nome e sobrenome de uma pessoa separadamente e depois mostre eles juntos.()
+var nome1 = prompt("Digite o seu nome")
+    var nome2 = prompt("Digite o seu sobrenome")
+    alert(nome1 + nome2)
+
+//Escreva em JavaScript um programa que efetue as 4 operações aritméticas em sequência: Adição, Subtração, Multiplicação e Divisão (fazer variáveis diferentes para todas as operações)()
+
+ //tentativa de adiçao()
+ var numero1 = Number.parseInt(prompt("digite o primeiro numero"))
+ var numero2 = Number.parseInt(prompt("digite o segundo numero"))
+
+//verificando o tipo()
+ console.log(typeof(numero1))
+ alert(numero1 + numero2)
+ console.log(typeof(numero1))
+ alert(numero1 - numero2)
+ console.log(typeof(numero1))
+ alert(numero1 / numero2)
+ console.log(typeof(numero1))
+ alert(numero1 * numero2)
+ 
+ //Escreva em JavaScript um programa que retorne o cubo de um número fornecido pelo usuário.
+ var numero = Number.parseInt(prompt("digite um numero"))
+ console.log(typeof(numero))
+ alert(numero * 3)
+
+//Escreva em JavaScript um programa que realize a conversão de uma temperatura fornecida em graus Fahrenheit (F) para Celsius (C).
+var numerograus = Number.parseInt(prompt("digite um numero"))
+ console.log(typeof(numerograus))
+ alert(C = 5/9 * (numerograus - 32))
+
+ //Escreva em JavaScript um programa que informe o retorno de um investimento (montante) com base nos valores do capital inicial, tempo em meses e taxa de juros mensal, fornecidos pelo usuário.
+
+ var capital1 = Number.parseInt(prompt("digite o capital inicial"))
+ var taxajuros = Number.parseInt(prompt("digite a taxa de juros"))
+ var investimento = Number.parseInt(prompt("digite o tempo de investimento"))
+ console.log(typeof(capital1))
+ alert(m = capital1 * (1 + taxajuros * investimento))
+
+ 
---
+++ Javascript/aula-01/readme.md
@@ -0,0 +1,3 @@
+Aula introdutoria sobre Java script
+comandos basicos relacionados a comandos de alerta,comfirmaçao
+contas de matemática basica,caracteristicas e informações gerais sobre Javascript
